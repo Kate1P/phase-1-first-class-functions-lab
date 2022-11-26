58523045330955d956196d17286b86bc8b575769
Kate Popova <popova1ea@gmail.com>
2022-11-26 00:21:35 -0800

    commit tree 804a951eb282a72a19f9eaedc114a47829009416

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arrayOfDrivers => {
+    const firstTwoDrivers = [arrayOfDrivers[0], arrayOfDrivers[1]];
+    return firstTwoDrivers;
+}
+const returnLastTwoDrivers = arrayOfDrivers => {
+    const lastTwoDrivers = [arrayOfDrivers[2], arrayOfDrivers[3]];
+    return lastTwoDrivers;
+}
+
